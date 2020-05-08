3f3c9f8447e41f2baed5b5d3caee8ea56d786686
manggong <jihwank1995@gmail.com>
2020-05-08 13:21:49 +0900

    Add email address, about

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => (
   <>
-    <h1>Edit About component or pages/about.js to include your information.</h1>
+    <h1>김지환 기술블로그 입니다.</h1>
+    <br />
+    <h1>어서오세요!!!</h1>
   </>
-)
+);
 
-export default About
+export default About;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import Layout from '../layout'
-import config from '../../data/SiteConfig'
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../layout";
+import config from "../../data/SiteConfig";
 
 const ContactPage = () => (
   <Layout>
     <main>
       <Helmet title={`Contact | ${config.siteTitle}`} />
       <h1>Contact us</h1>
-      <p>Contact informations</p>
+      <p>jihwan.k1995@gmail.com</p>
     </main>
   </Layout>
-)
-export default ContactPage
+);
+export default ContactPage;
